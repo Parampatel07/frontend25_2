445d31f3189fb3ceed11cebc0be62b70dbfb0cb6
parampatel07 <iamparam102@gmail.com>
2024-01-05 12:01:28 +0530

    node

--- express/lesson8.js
+++ express/lesson8.js
@@ -4,20 +4,16 @@
 var express = require('express');
 var app = express();
 var bodyParese = require('body-parser');
-
 app.use(express.json());
 app.post("/login", function (request, response) {
-
      let email = request.body.email;
      let password = request.body.password;
-
      if (email === "demo@gmail.com" && password === "123123") {
           response.send("Login Successfully ");
      }
      else {
-          response.send("Login Invalid ");   
+          response.send("Login Invalid ");
      }
-
 });
 app.listen(5000);
 console.log("Server ready ");
