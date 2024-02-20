0be7a3d2c048f9269b2d79aeecf86fa5d068084f
parampatel07 <iamparam102@gmail.com>
2024-02-20 11:56:09 +0530

    example 4

--- reactjs/app_one/src/example4.jsx
+++ reactjs/app_one/src/example4.jsx
@@ -1,21 +1,123 @@
 import React from "react";
 import { Tooltip } from "react-tooltip";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 export default class Theater extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectSeat: 0,
+      showBill: 0,
+    };
+  }
+  changeSeat = (price) => {
+    console.log(price);
+    this.setState({
+      selectSeat: price,
+    });
+  };
+
+  message = () =>
+    toast.success("Seat Booked Successfully !", {
+      position: "bottom-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
+  displayBill(price) {
+    return (
+      <div className="col-4 mt-5">
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          
+        />
+        <div className="card mt-5 shadow">
+          <div className="card-header text-bg-success">
+            <h2 className="text-center mt-4 ">Booked Seats</h2>
+          </div>
+          <div className="card-body p-3">
+            <table
+              className="table table-striped table-hover table-bordered"
+              cellPadding={5}
+            >
+              <thead>
+                <th>Seat</th>
+                <th>price</th>
+              </thead>
+              <tr>
+                <td>Seat 1</td>
+                <td>{price}</td>
+              </tr>
+            </table>
+            <div className="text-center">
+              <button className="btn btn-primary mt-3" onClick={this.message}>
+                Pay Now
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  handleBookNow = (status) => {
+    console.log("check");
+    if (this.state.selectSeat !== 0) {
+      this.setState({
+        showBill: status,
+      });
+    }
+  };
+
   render() {
     return (
       <>
-        <div className="container mt-5">
+        <div className="container-fluid mt-5">
           <div className="row">
-            <div className="col-10 offset-1">
-              <div className="card">
-                <div className="card-body text-center text-bg-success">
-                  <h3>Screen</h3>
+            <div className="col-8">
+              <div className="row">
+                <div className="col-12 ">
+                  <div className="card">
+                    <div className="card-body text-center text-bg-success">
+                      <div className="row">
+                        <div className="col-10 text-center">
+                          <h3>Screen - {this.state.selectSeat}</h3>
+                        </div>
+                        <div className="col-2">
+                          <button
+                            className="btn btn-primary"
+                            onClick={() => this.handleBookNow(1)}
+                          >
+                            Book Now
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <Line selectSeat={this.changeSeat} price="200"></Line>
+                  <Line selectSeat={this.changeSeat} price="250"></Line>
+                  <Line selectSeat={this.changeSeat} price="400"></Line>
                 </div>
               </div>
-              <Line price="200"></Line>
-              <Line price="250"></Line>
-              <Line price="400"></Line>
             </div>
+            {this.state.showBill == 0
+              ? ""
+              : this.displayBill(this.state.selectSeat)}
           </div>
         </div>
       </>
@@ -24,22 +126,61 @@ export default class Theater extends React.Component {
 }
 
 class Line extends React.Component {
+  changeSeat = (price) => {
+    this.props.selectSeat(price);
+  };
   render() {
     return (
       <>
         <div className="row mt-5">
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
-          <Seats content={this.props.price}></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
+          <Seats
+            content={this.props.price}
+            selectSeat={this.changeSeat}
+          ></Seats>
         </div>
       </>
     );
@@ -50,6 +191,9 @@ class Seats extends React.Component {
   constructor(props) {
     super(props);
   }
+  setSeat(price) {
+    this.props.selectSeat(price);
+  }
   render() {
     return (
       <>
@@ -58,7 +202,11 @@ class Seats extends React.Component {
           data-tooltip-id="1"
           data-tooltip-content={this.props.content}
         >
-          <div className="card">
+          <div
+            className="card"
+            id="seat"
+            onClick={() => this.setSeat(this.props.content)}
+          >
             <div className="card-body text-center">
               <img
                 src="image/seat.png"
